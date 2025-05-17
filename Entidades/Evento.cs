0161c57483f283d4a2a866437b93204eeb672ece
Gaston Pandullo <gpandullo3@gmail.com>
2025-05-17 18:10:15 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Evento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invitacion.Entidades
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public DateTime Fecha { get; set; }
+        public string Ubicacion { get; set; } = string.Empty;
+        public bool Realizado { get; set; }
+    }
+}
---
+++ Entidades/Invitado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invitacion.Entidades
+{
+    internal class Invitado
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; } = string.Empty;
+        public bool Confirmado { get; set; }
+    }
+}
